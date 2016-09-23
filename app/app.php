73eb11346c24792c13b81a284be56d792c7f12c2
Ryan Apking <ryan.apking@gmail.com>
2016-09-23 11:06:08 -0700

    update Silex routes to include clients page.

--- app/app.php
+++ app/app.php
@@ -39,17 +39,26 @@
         return $app->redirect('/');
     });
 
-    $app->get('/clients/{client_id}', function($client_id) use ($app) {
-        $client = Client::find($client_id);
-        return $app['twig']->render('clients.html.twig', array('client' => $client));
+    $app->get('/clients/{stylist_id}', function($stylist_id) use ($app) {
+        $clients = Client::findByStylistId($stylist_id);
+        $stylist = Stylist::find($stylist_id);
+        return $app['twig']->render('clients.html.twig', array('clients' => $clients, 'stylist' => $stylist));
     });
 
-    $app->post('/clients', function() use ($app) {
+
+
+
+
+    $app->get('/client_edit/{client_id}', function($client_id) use ($app) {
+        $client = Client::find($client_id);
+        return $app['twig']->render('client_edit.html.twig', array('client' => $client));
+    });
+
+    $app->post('/client_edit/{stylist_id}', function($stylist_id) use ($app) {
         $name = $_POST['client_name'];
-        $stylist_id = $_POST['stylist_id'];
         $new_client = new Client($name, $stylist_id);
         $new_client->save();
-        return $app->redirect('/');
+        return $app->redirect('/clients/' . $stylist_id);
     });
 
     $app->patch('/clients/{client_id}', function($client_id) use ($app) {
